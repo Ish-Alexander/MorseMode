0b743b3eb43b33c6ff0792f38956e268eefbb290
Ishauna Alexander <koolwaterishgem36@gmail.com>
2026-01-22 09:55:44 -0500

    Initial Commit

---
+++ MorseMode Watch App/MorseModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseModeApp.swift
+//  MorseMode Watch App
+//
+//  Created by Ishauna Marie Alexander on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MorseMode_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorseMode/MorseModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseModeApp.swift
+//  MorseMode
+//
+//  Created by Ishauna Marie Alexander on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MorseModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
